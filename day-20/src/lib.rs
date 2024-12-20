ec20311cd0db23d3a6b39391bf1b9ea33a6ea513
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2024-12-20 17:54:28 -0500

    pt 2 wip

--- day-20/src/lib.rs
+++ day-20/src/lib.rs
@@ -6,6 +6,8 @@ use std::collections::HashSet;
 const DATA: &str = include_str!("../input.txt");
 const DIRECTIONS: [(i32, i32); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)]; // y, x
 const MIN_CHEAT: usize = 100;
+#[cfg(feature = "part2")]
+const CHEAT_LENGTH: usize = 20;
 
 #[derive(GridCell, Copy, Clone, Debug, PartialEq, Eq, Default)]
 pub enum Cell {
@@ -38,10 +40,10 @@ pub type Consequent = usize;
 
 fn get_key_points(
   grid: &Grid<Cell>,
-) -> Result<(Point, Point, Vec<Point>, Vec<Point>), String> {
+) -> Result<(Point, Point, HashSet<Point>, Vec<Point>), String> {
   let mut start: Option<Point> = None;
   let mut end: Option<Point> = None;
-  let mut interesting_walls = Vec::new();
+  let mut interesting_walls = HashSet::new();
 
   for (position, cell) in grid.iter() {
     match cell {
@@ -62,7 +64,7 @@ fn get_key_points(
           }
         });
         if count > 1 {
-          interesting_walls.push(position);
+          interesting_walls.insert(position);
         }
       }
       _ => (),
@@ -101,56 +103,7 @@ fn successors(p: Point, grid: &Grid<Cell>) -> Vec<Point> {
     .collect()
 }
 
-fn find_all_shorter_paths(
-  grid: &Grid<Cell>,
-) -> Result<(Vec<Vec<Point>>, usize), String> {
-  let (_start, _end, interesting_walls, normal_path) = get_key_points(grid)?;
-
-  let mut cheat_paths = Vec::new();
-  for wall in interesting_walls {
-    if let Some(points) = get_path_neighbors(wall, &normal_path) {
-      // Use itertools to get all distinct pairs of points
-      for combination in points.iter().combinations(2) {
-        if let [start_point, end_point] = &combination[..] {
-          let start_idx = normal_path
-            .iter()
-            .position(|&p| p == **start_point)
-            .ok_or("Start point not in normal path")?;
-          let end_idx = normal_path
-            .iter()
-            .position(|&p| p == **end_point)
-            .ok_or("End point not in normal path")?;
-
-          let (start_idx, end_idx) = if start_idx < end_idx {
-            (start_idx, end_idx)
-          } else {
-            (end_idx, start_idx)
-          };
-
-          if end_idx - start_idx > 2 {
-            let cheat_path_length =
-              start_idx + 1 + 1 + (normal_path.len() - end_idx);
-            let mut cheat_path = Vec::with_capacity(cheat_path_length);
-            cheat_path.extend(normal_path.iter().take(start_idx + 1));
-            cheat_path.push(wall);
-            cheat_path.extend(normal_path.iter().skip(end_idx));
-
-            cheat_paths.push(cheat_path);
-          }
-        }
-      }
-    } else {
-      return Err("No neighbors of interesting wall found".to_string());
-    }
-  }
-
-  if !cheat_paths.is_empty() {
-    Ok((cheat_paths, normal_path.len()))
-  } else {
-    Err("No shorter paths found".to_string())
-  }
-}
-
+#[cfg(not(feature = "part2"))]
 fn get_path_neighbors(point: Point, path: &[Point]) -> Option<HashSet<Point>> {
   let mut neighbors = HashSet::new();
 
@@ -168,6 +121,151 @@ fn get_path_neighbors(point: Point, path: &[Point]) -> Option<HashSet<Point>> {
   }
 }
 
+#[cfg(not(feature = "part2"))]
+fn add_cheat_paths(
+  normal_path: &[Point],
+  cheat_paths: &mut Vec<Vec<Point>>,
+  wall: Point,
+  points: HashSet<Point>,
+  distance: usize,
+) -> Result<(), String> {
+  #[allow(clippy::unit_arg)]
+  Ok(for combination in points.iter().combinations(2) {
+    if let [start_point, end_point] = &combination[..] {
+      let start_idx = normal_path
+        .iter()
+        .position(|&p| p == **start_point)
+        .ok_or("Start point not in normal path")?;
+      let end_idx = normal_path
+        .iter()
+        .position(|&p| p == **end_point)
+        .ok_or("End point not in normal path")?;
+
+      let (start_idx, end_idx) = if start_idx < end_idx {
+        (start_idx, end_idx)
+      } else {
+        (end_idx, start_idx)
+      };
+
+      if end_idx - start_idx > 2 + distance {
+        let cheat_path_length =
+          start_idx + 1 + 1 + (normal_path.len() - end_idx);
+        let mut cheat_path = Vec::with_capacity(cheat_path_length);
+        cheat_path.extend(normal_path.iter().take(start_idx + 1));
+        cheat_path.push(wall);
+        cheat_path.extend(normal_path.iter().skip(end_idx));
+
+        cheat_paths.push(cheat_path);
+      }
+    }
+  })
+}
+
+#[cfg(feature = "part2")]
+fn process_interesting_pair(
+  p1: &Point,
+  p2: &Point,
+  normal_path: &[Point],
+  cheat_paths: &mut Vec<Vec<Point>>,
+) -> Result<(), String> {
+  let p1_idx = normal_path
+    .iter()
+    .position(|&p| p == *p1)
+    .ok_or("First point not found in normal path")?;
+
+  let p2_idx = normal_path
+    .iter()
+    .position(|&p| p == *p2)
+    .ok_or("Second point not found in normal path")?;
+  let (start_idx, end_idx) = if p1_idx < p2_idx {
+    (p1_idx, p2_idx)
+  } else {
+    (p2_idx, p1_idx)
+  };
+  let path_distance = end_idx - start_idx;
+
+  let direct_distance = manhattan_distance(p1, p2);
+  if direct_distance < path_distance {
+    let cheat_path_length = start_idx + 1 + (normal_path.len() - end_idx);
+    let mut cheat_path = Vec::with_capacity(cheat_path_length);
+
+    cheat_path.extend(normal_path.iter().take(start_idx + 1));
+    cheat_path.extend(normal_path.iter().skip(end_idx));
+
+    cheat_paths.push(cheat_path);
+  }
+
+  Ok(())
+}
+
+#[cfg(feature = "part2")]
+fn manhattan_distance(a: &Point, b: &Point) -> usize {
+  ((a.x - b.x).abs() + (a.y - b.y).abs()) as usize
+}
+
+#[cfg(not(feature = "part2"))]
+fn find_all_shorter_paths(
+  grid: &Grid<Cell>,
+) -> Result<(Vec<Vec<Point>>, usize), String> {
+  let (_start, _end, interesting_walls, normal_path) = get_key_points(grid)?;
+
+  let mut cheat_paths = Vec::new();
+  for wall in interesting_walls {
+    if let Some(points) = get_path_neighbors(wall, &normal_path) {
+      add_cheat_paths(&normal_path, &mut cheat_paths, wall, points, 0)?;
+    } else {
+      return Err("No neighbors of interesting wall found".to_string());
+    }
+  }
+
+  if !cheat_paths.is_empty() {
+    Ok((cheat_paths, normal_path.len()))
+  } else {
+    Err("No shorter paths found".to_string())
+  }
+}
+
+#[cfg(feature = "part2")]
+fn find_all_shorter_paths(
+  grid: &Grid<Cell>,
+) -> Result<(Vec<Vec<Point>>, usize), String> {
+  let (_start, _end, interesting_walls, normal_path) = get_key_points(grid)?;
+
+  let mut cheat_paths = Vec::new();
+  for start in normal_path.clone() {
+    let mut interesting_path = HashSet::new();
+    grid.iter().for_each(|(p, c)| {
+      if c == Cell::Wall
+        && interesting_walls.contains(&p)
+        && manhattan_distance(&start, &p) <= CHEAT_LENGTH
+      {
+        DIRECTIONS.iter().for_each(|(dy, dx)| {
+          let p_prime = Point { x: p.x + dx, y: p.y + dy };
+          if grid.is_in_bounds(p_prime) {
+            match grid[p_prime] {
+              Cell::Wall => (),
+              _ => {
+                interesting_path.insert(p_prime);
+              }
+            }
+          }
+        });
+      }
+    });
+    // consider the distances between each pait of interesting path points. if it is less than it was in the normal path, add it to the cheat path much as cheat paths are added in the add_chewat_path part 1 function
+    for (p1, p2) in interesting_path.iter().tuple_combinations() {
+      process_interesting_pair(p1, p2, &normal_path, &mut cheat_paths)?;
+    }
+  }
+
+  if !cheat_paths.is_empty() {
+    Ok((cheat_paths, normal_path.len()))
+  } else {
+    Err("No shorter paths found".to_string())
+  }
+}
+
+#[cfg(feature = "debug")]
 fn print_path_grid(grid: &Grid<Cell>, path: &[Point], cost: usize) {
   let mut display_grid = vec![vec!['.'; grid.width()]; grid.height()];
 
@@ -217,7 +315,8 @@ fn src_provider() -> Result<String, String> {
 }
 
 pub mod prelude {
-  //use crate::print_path_grid;
+  #[cfg(feature = "debug")]
+  use crate::print_path_grid;
   use crate::{
     find_all_shorter_paths, get_min_cheat, src_provider, Consequent,
     ProblemDefinition,
@@ -239,7 +338,8 @@ pub mod prelude {
         .iter()
         .filter_map(|cheat_path| {
           if cheat_path.len() + min_cheat <= normal_cost {
-            //print_path_grid(&data, cheat_path, cheat_path.len());
+            #[cfg(feature = "debug")]
+            print_path_grid(&data, cheat_path, cheat_path.len());
             Some(())
           } else {
             None
@@ -285,7 +385,7 @@ mod tests {
     assert_eq!(result, Ok(44));
   }
 
-  #[cfg(not(feature = "part2"))]
+  #[cfg(feature = "part2")]
   #[test]
   #[mry::lock(get_min_cheat)]
   #[mry::lock(src_provider)]
